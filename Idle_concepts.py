20a66d43e64b74ba84c4124863ae279690653776
KhushiThakur25 <113238209+KhushiThakur25@users.noreply.github.com>
2025-03-10 14:40:01 +0530

    Add files via upload

---
+++ Idle_concepts.py
@@ -0,0 +1,87 @@
+Python 3.13.0 (tags/v3.13.0:60403a5, Oct  7 2024, 09:38:07) [MSC v.1941 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================= RESTART: E:/Mishti_python/class_1.py =================
+hello world..
+sum of x and y is :  11
+['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+False = "yes"
+SyntaxError: cannot assign to False
+1name = "int"
+SyntaxError: invalid decimal literal
+name1 = "hello"
+name1
+'hello'
+x = 0
+_name = "om"
+_name
+'om'
+name@ = "aman"
+SyntaxError: invalid syntax
+@hi = 56
+SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
+name = "Khushi"
+Name
+Traceback (most recent call last):
+  File "<pyshell#10>", line 1, in <module>
+    Name
+NameError: name 'Name' is not defined. Did you mean: 'name'?
+5/2
+2.5
+5//2
+2
+5%2
+1
+5**2
+25
+2**10
+1024
+a = 5
+b = 6
+c = 5
+a == c
+True
+a ==b
+False
+a != b
+True
+a<b
+True
+a > c
+False
+a <= c
+True
+a >= b
+False
+x = 50
+x + 10
+60
+x
+50
+x = x + 10
+x
+60
+x += 10
+x
+70
+x /= 10
+x
+7.0
+x -= 1
+x
+6.0
+>>> x *= 6
+>>> x
+36.0
+>>> x // 1
+36.0
+>>> x //= 10
+>>> x
+3.0
+>>> 
+================= RESTART: E:/Mishti_python/class_1.py =================
+sum of x and y is :  11
+['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+>>> 
+================= RESTART: E:/Mishti_python/class_1.py =================
+sum of x and y is :  11
---
+++ class_1.py
@@ -0,0 +1,20 @@
+#print("hello world..")
+'''
+import keyword
+
+print(keyword.kwlist)'''
+
+x = 5
+y = 6
+
+print("sum of x and y is : ",x+y)
+
+print("sum of",x,"and",y,"is :",x+y)
+'''
+print("sum of %d and %d is : %d"%(x,y,x+y))
+
+print("div of %d and %d is : %f"%(x,y,x/y))
+'''
+print("div of {} and {} is: {}".format(x,y,x/y))
+
+print(f"div of {x} and {y} is: {x/y}")
